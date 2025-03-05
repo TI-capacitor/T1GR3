9d017097e24eb2094f082808a8f2447b040077a1
Sebastian Elliott Pedrosa <pedrosaenterprises@gmail.com>
2025-03-05 00:51:09 -0400

    removed problematic build files from template_ws folder

--- template_ws/build/ros_gz_example_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sebastian-elliott-pedrosa/T1GR3/template_ws/src/ros_gz_project_template/ros_gz_example_bringup
-# Build directory: /home/sebastian-elliott-pedrosa/T1GR3/template_ws/build/ros_gz_example_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- template_ws/install/ros_gz_example_bringup/share/ros_gz_example_bringup/cmake/ros_gz_example_bringupConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/sebastian-elliott-pedrosa/T1GR3/template_ws/build/ros_gz_example_bringup/ament_cmake_core/ros_gz_example_bringupConfig-version.cmake
